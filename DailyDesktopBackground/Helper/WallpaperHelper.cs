d6045c7c2103485911b31e3af716bcbcf6d18590
Linus Eckhard <l.eckhard@interfacema.de>
2020-06-17 11:05:08 +0200

    Add Microsoft.Win32.Registry and Magick.NET dependency, implement WallpaperHelper

---
+++ DailyDesktopBackground/Helper/WallpaperHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using ImageMagick;
+using Microsoft.Win32;
+
+namespace DailyDesktopBackground.Helper
+{
+    public class WallpaperHelper
+    {
+        /// <summary>
+        /// uiAction Desktop parameter for setting the desktop wallpaper.
+        /// </summary>
+        const int SPI_SETDESKWALLPAPER = 0x0014;
+
+        /// <summary>
+        /// fWinIni parameter to save the changed setting in the user profile.
+        /// </summary>
+        const int SPIF_UPDATEINIFILE = 0x01;
+
+        /// <summary>
+        /// fWinIni parameter to send a WM_SETTINGCHANGE message after updating the user profile.
+        /// </summary>
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        /// <summary>
+        /// This wallpaper style enlarges/shrinks the image keeping the original aspect ratio to fit the screen.
+        /// </summary>
+        const int WALLPAPER_STYLE_FILL = 10;
+
+        /// <summary>
+        /// External call to user32.dll that can set a variety of system parameters, in this case we
+        /// use it to set the desktop wallpaper.
+        /// </summary>
+        /// <param name="uiAction">Specifies the parameter we are setting, in this case SPI_SETDESKWALLPAPER</param>
+        /// <param name="uiParam">Used by some uiAction parameters, in this case we always use 0</param>
+        /// <param name="pvParam"></param>
+        /// <param name="fWinIni"></param>
+        /// <returns>When setting the desktop wallpaper, this always returns true unless there is an error.</returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uiAction, int uiParam, string pvParam, int fWinIni);
+
+        /// <summary>
+        /// Changes the current user's desktop wallpaper by replacing the old one and overwriting the registry.
+        /// The wallpaper will be saved in the bmp format with no tiling and a "Fill" wallpaper style.
+        /// </summary>
+        /// <param name="uri">Uri to the new desktop wallpaper</param>
+        public static void SetWallpaper(Uri uri)
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            using (var image = DownloadImage(uri))
+            {
+                image.Write(tempPath, MagickFormat.Bmp);
+            }
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", WALLPAPER_STYLE_FILL.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        private static MagickImage DownloadImage(Uri uri)
+        {
+            using (var stream = new System.Net.WebClient().OpenRead(uri.ToString()))
+            {
+                return new MagickImage(stream);
+            }
+        }
+    }
+}
--- DailyDesktopBackground/Program.cs
+++ DailyDesktopBackground/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using DailyDesktopBackground.Helper;
+using System;
 
 namespace DailyDesktopBackground
 {
     class Program
     {
+        private const string _wallpaper1 =
+            "https://images.unsplash.com/photo-1588288522163-375103610bbc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE0MjcwNn0&w=1920";
+        private const string _wallpaper2 =
+            "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjcwNn0";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            WallpaperHelper.SetWallpaper(
+                new Uri(_wallpaper2));
         }
     }
 }
