5aa0600ab172d407938e365937d509d142f4322a
Linus Eckhard <l.eckhard@interfacema.de>
2020-06-17 09:48:51 +0200

    init c# solution

---
+++ DailyDesktopBackground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyDesktopBackground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
